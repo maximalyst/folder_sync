2072609f06c1667597b35994e7adc56abe7ad326
Brandon Smith <maximalyst@users.noreply.github.com>
2016-04-09 17:51:58 -0400

    Final working version
    Edited offline, confirmed working locally.

--- monitordropboxnotes.py
+++ monitordropboxnotes.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
 import hashlib
 import os
 import shutil
+import plistlib
+
+
 
 class HashMe:
     '''Class for making hashed file objects and checksum files'''
@@ -39,96 +41,111 @@ def copyFile(sourceDirectory, targetDirectory):
 
     shutil.copy2(sourceDirectory, targetDirectory)
 
+dirList = []
+fileList = []
 
+# Working on reading parent and target directories from settings plist file...
+with open('/Library/LaunchAgents/com.bstudios.monitordropboxnotes.plist', mode='rb') as f:
+    settings = plistlib.load(f)
+    originalParent = settings["syncParentNames"]
+    originalParentName = [] # for holding the bare directory name of the parent folder
+    for i, path in enumerate(originalParent):
+        originalParentName.append(path.rsplit('/')[-1]) # JUST the name of the parent folder
+    targetParent = settings["syncTargetName"]
+    fileType = tuple(settings["fileTypes"]) # uses a tuple rather than a list for looping through within endswith
 
+#originalParent = '/Users/brandon/Programming/testing'
 
-
-
-originalParent = '/Users/brandon/Programming/testing'
-originalParentName = originalParent.rsplit('/')[-1] # JUST the name of the parent folder
 # Target directory for all files and subfolders to copy to:
-targetParent = '/Users/brandon/Programming/testing2'
-fileType = '.txt'
+# targetParent = '/Users/brandon/Programming/testing2'
 
-if not fileType.startswith('.'):
-    fileType = '.' + fileType
+for i, type in enumerate(fileType):
+    if not type.startswith('.'):
+        fileType[i] = '.' + type
 
 
-checksumParentFolder = originalParent+'/checksums'
+checksumParentFolders = tuple([oP + '/checksums' for oP in originalParent])
 # Just in case there isn't a checksums folder in the directory we're following:
-if not(os.path.isdir(checksumParentFolder)):
-    os.makedirs(checksumParentFolder, exist_ok=True)
+for cspf in checksumParentFolders:
+    if not(os.path.isdir(cspf)):
+        os.makedirs(cspf, exist_ok=True)
     # condition eliminates a possible race condition, so it's a bit safer. Better safe than sorry. Thanks Python 3.
 
-
+# ^^^^^^^^COMPLETE TO THIS POINT^^^^^^^
 # Generate lists of Directories and Subdirectories, and all files within:
-dirList = [x[0] for x in os.walk(originalParent)]
-# folderList = [x[1] for x in os.walk(originalParent)] # don't need this line
-fileList = [x[2] for x in os.walk(originalParent)]
+for i, op in enumerate(originalParent):
+    dirList.append([x[0] for x in os.walk(op)])
+    # folderList = [x[1] for x in os.walk(originalParent)] # don't need this line
+    fileList.append([x[2] for x in os.walk(op)])
 
-#Need to remove files that aren't of the type we're looking for
+# Need to remove files that aren't of the type we're looking for
+# Given that fileList is a 3-deep list, need to dig down a little to get into the lowest level.
 for subfileList in fileList:
-    temp = subfileList.copy() # Python passes by reference; force pass by copy
-    for file in temp:
-        if not file.endswith(fileType):
-            subfileList.remove(file)
+    for subsubfileList in subfileList:
+        temp = subsubfileList.copy() # Python passes by reference; force pass by copy
+        for file in temp:
+            if not(file.endswith(fileType)):
+                subsubfileList.remove(file)
 
 
 
-for i, folder in enumerate(dirList):
-    if folder.rsplit(checksumParentFolder)[0] == '': # Ignore the checksums folder!
-        continue
+for kk, superfolder in enumerate(dirList):
+    for i, folder in enumerate(superfolder):
+        if folder in checksumParentFolders:  # ignore the checksum folder...
+            continue
 
-    # looping through the files within each full path that we have.
+        # looping through the files within each full path that we have.
 
-    # strip off the path back to the parent folder name. It has a / at the front!!
-    folderName = folder.rsplit(originalParentName)[-1]
+        # path of current folder, up to parent name.
+        folderName = originalParentName[kk] + superfolder[0].rsplit(originalParentName[kk])[-1]
 
-    # Full path for the current folder's checksum folder
-    checksumFolder = checksumParentFolder + folder.rsplit('/'+originalParentName)[-1]
+        # Full path for the current folder's checksum folder
+        checksumFolder = superfolder[0] + '/checksums'
 
-    # make checksum folder for the current source folder
-    if not(os.path.isdir(checksumFolder)):
-        os.makedirs(checksumFolder, exist_ok=True)
+        # make checksum folder for the current source folder
+        if not(os.path.isdir(checksumFolder)):
+            os.makedirs(checksumFolder, exist_ok=True)
 
-    for file in fileList[i]:
+        for subfileList in fileList[kk]:
+            for file in subfileList:
+                myfileType = '.' + file.rsplit('.')[-1]  #get current file's extension
 
-        #hash each file
-        hashedFile = HashMe(folder+'/'+file)
-        hashedFile.hash_file()
+                #hash each file
+                hashedFile = HashMe(folder+'/'+file)
+                hashedFile.hash_file()
 
-        # If we're at this point, the file is definitely a .pdf.
+                # If we're at this point, the file is definitely a kind we're looking for.
 
-        # save a file with hash function as the same name, different extension
-        hashFileName = file[:len(fileType)] + '.md5' #remove filetype, add .md5
+                # save a file with hash function as the same name, different extension
+                hashFileName = file.rsplit(myfileType)[0] + '.md5' #remove filetype, add .md5
 
 
-        if not(os.path.isfile(checksumFolder+'/'+hashFileName)):
-        # New file since last check; need to generate md5 checksum file and copy
-           #the file to the target directory
-            with open(checksumFolder+'/'+hashFileName, 'w') as saveFile:
-                saveFile.write(hashedFile.hashvalue)
+                if not(os.path.isfile(checksumFolder+'/'+hashFileName)):
+                # New file since last check; need to generate md5 checksum file and copy
+                   #the file to the target directory
+                    with open(checksumFolder+'/'+hashFileName, 'w') as saveFile:
+                        saveFile.write(hashedFile.hashvalue)
 
-                # Copy file to disk, making new directory if necessary
-                #copyFile(folder+'/'+file, targetParent+folderName)
-                if not(os.path.isdir(targetParent+folderName)):
-                    os.makedirs(targetParent+folderName, exist_ok=True)
-                shutil.copy2(folder+'/'+file, targetParent+folderName)
+                        # Copy file to disk, making new directory if necessary
+                        #copyFile(folder+'/'+file, targetParent+folderName)
+                        if not(os.path.isdir(targetParent+'/'+folderName)):
+                            os.makedirs(targetParent+'/'+folderName, exist_ok=True)
+                        shutil.copy2(folder+'/'+file, targetParent+'/'+folderName)
 
-        else: # file already exists, so compare old sum to new sum
-            with open(checksumFolder+'/'+hashFileName, 'r+') as readFile:
+                else: # file already exists, so compare old sum to new sum
+                    with open(checksumFolder+'/'+hashFileName, 'r+') as readFile:
 
-                md5check = readFile.read()
+                        md5check = readFile.read()
 
-                if md5check != hashedFile.hashvalue:
-                    # file is different than the last time we looked at it.
-                    # insert the new hash value, truncate the old
-                    readFile.seek(0)
-                    readFile.write(hashedFile.hashvalue)
-                    readFile.truncate()
+                        if md5check != hashedFile.hashvalue:
+                            # file is different than the last time we looked at it.
+                            # insert the new hash value, truncate the old
+                            readFile.seek(0)
+                            readFile.write(hashedFile.hashvalue)
+                            readFile.truncate()
 
-                    # now save the new file locally, copying with metadata...
-                    #copyFile(folder+'/'+file, targetParent+folderName)
-                    if not(os.path.isdir(targetParent+folderName)):
-                        os.makedirs(targetParent+folderName, exist_ok=True)
-                    shutil.copy2(folder+'/'+file, targetParent+folderName)
+                            # now save the new file locally, copying with metadata...
+                            #copyFile(folder+'/'+file, targetParent+folderName)
+                            if not(os.path.isdir(targetParent+'/'+folderName)):
+                                os.makedirs(targetParent+'/'+folderName, exist_ok=True)
+                            shutil.copy2(folder+'/'+file, targetParent+'/'+folderName)
